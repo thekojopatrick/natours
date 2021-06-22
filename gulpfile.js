309641d944151c30a29759307a2cbb66c0e0b8ae
Kojo Patrick <patrickasare84@gmail.com>
2021-06-22 15:48:52 +0000

    complete natours app v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,66 @@
 // Gulp loader
-const {
-  src,
-  dest,
-  watch,
-  series,
-} = require('gulp');
-
+const { src, dest, watch, series } = require('gulp')
 
 // --------------------------------------------
 // Dependencies
 // --------------------------------------------
 
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const terser = require('gulp-terser');
-const browsersync = require('browser-sync').create();
+const sass = require('gulp-sass')
+const postcss = require('gulp-postcss')
+const cssnano = require('cssnano')
+const terser = require('gulp-terser')
+const autoprefixer = require('autoprefixer')
+const browsersync = require('browser-sync').create()
 
 // Images plugins
-let images = require('gulp-imagemin');
+let images = require('gulp-imagemin')
 
 // Sass Task
 function scssTask() {
-  return src('src/scss/style.scss', { sourcemaps: true })
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('dist/assets/css', { sourcemaps: '.' }));
+	return src('src/scss/style.scss', { sourcemaps: true })
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss([autoprefixer(), cssnano()]))
+		.pipe(dest('dist/assets/css', { sourcemaps: '.' }))
 }
 
 // JavaScript Task
 function jsTask() {
-  return src('src/js/script.js', { sourcemaps: true })
-    .pipe(terser())
-    .pipe(dest('dist/assets/js', { sourcemaps: '.' }));
+	return src('src/js/script.js', { sourcemaps: true })
+		.pipe(terser())
+		.pipe(dest('dist/assets/js', { sourcemaps: '.' }))
 }
 
 // Images
 function img() {
- return src('src/img/*')
-    .pipe(images())
-    .pipe(dest('dist/assets/img'));
-  
-};
+	return src('src/img/*').pipe(images()).pipe(dest('dist/assets/img'))
+}
 
 //Vendors & Plugins Tasks
 
-
-
 // Init Browser Watch
 function browsersyncServe(cb) {
-  browsersync.init({
-    server: {
-      baseDir: './dist'
-    }
-  });
-  cb();
+	browsersync.init({
+		server: {
+			baseDir: './dist',
+		},
+	})
+	cb()
 }
 
 function browsersyncReload(cb) {
-  browsersync.reload();
-  cb();
+	browsersync.reload()
+	cb()
 }
 
 // Watch Task
 function watchTask() {
-  // Serve files from the root of this project
-  watch('dist/*.html', browsersyncReload);
-  watch(['src/**/*.scss', 'src/**/*.js','src/img/*'], series(scssTask, jsTask, browsersyncReload));
+	// Serve files from the root of this project
+	watch('dist/*.html', browsersyncReload)
+	watch(
+		['src/**/*.scss', 'src/**/*.js', 'src/img/*'],
+		series(scssTask, jsTask, browsersyncReload)
+	)
 }
 
 // Default Gulp Task
-exports.default = series(
-  scssTask,
-  jsTask,
-  img,
-  browsersyncServe,
-  watchTask
-);
+exports.default = series(scssTask, jsTask, img, browsersyncServe, watchTask)
